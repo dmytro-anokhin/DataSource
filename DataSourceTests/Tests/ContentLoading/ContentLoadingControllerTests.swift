1effd64acda52ddf1d321af38cec79b592b4adac
Dmytro Anokhin <dmytro.anokhin@gmail.com>
2016-08-15 22:41:46 +0200

    Add unit test for components of content loading

---
+++ DataSourceTests/Tests/ContentLoading/ContentLoadingControllerTests.swift
@@ -0,0 +1,130 @@
+//
+//  ContentLoadingControllerTests.swift
+//  DataSource
+//
+//  Created by Dmytro Anokhin on 13/08/16.
+//  Copyright © 2016 Dmytro Anokhin. All rights reserved.
+//
+
+import XCTest
+@testable import DataSource
+
+
+class ClosureContentLoadingControllerDelegate : ContentLoadingControllerDelegate {
+
+    var willBeginLoading: ((controller: ContentLoadingController) -> Void)?
+    var didFinishLoadingWithUpdate: ((controller: ContentLoadingController, update: () -> Void) -> Void)?
+
+    init(willBeginLoading: ((controller: ContentLoadingController) -> Void)? = nil,
+        didFinishLoadingWithUpdate: ((controller: ContentLoadingController, update: () -> Void) -> Void)? = nil)
+    {
+        self.willBeginLoading = willBeginLoading
+        self.didFinishLoadingWithUpdate = didFinishLoadingWithUpdate
+    }
+
+    func contentLoadingControllerWillBeginLoading(_ controller: ContentLoadingController) {
+        willBeginLoading?(controller: controller)
+    }
+    
+    func contentLoadingController(_ controller: ContentLoadingController, didFinishLoadingWithUpdate update: () -> Void) {
+        didFinishLoadingWithUpdate?(controller: controller, update: update)
+    }
+}
+
+
+class ContentLoadingControllerTests: XCTestCase {
+
+    func testLoadContent() {
+
+        let willBeginLoadingExpectation = expectation(description: "Will begin loading")
+        let didFinishLoadingWithUpdateExpectation = expectation(description: "Did finish loading with update")
+        
+        let controller = ContentLoadingController()
+        XCTAssertEqual(controller.loadingState, .initial)
+        
+        let delegate = ClosureContentLoadingControllerDelegate(
+            willBeginLoading: { controller in
+                willBeginLoadingExpectation.fulfill()
+            },
+            didFinishLoadingWithUpdate: { controller, update in
+                didFinishLoadingWithUpdateExpectation.fulfill()
+            })
+
+        controller.delegate = delegate
+        
+        controller.loadContent { (coordinator) in
+            coordinator.done()
+        }
+        
+        XCTAssertEqual(controller.loadingState, .loadingContent)
+        
+        waitForExpectations(timeout: 0.1) { error in
+            let _ = delegate // Keep strong reference
+            XCTAssertEqual(controller.loadingState, .contentLoaded)
+        }
+    }
+    
+    func testError() {
+
+        let willBeginLoadingExpectation = expectation(description: "Will begin loading")
+        let didFinishLoadingWithUpdateExpectation = expectation(description: "Did finish loading with error")
+        
+        let controller = ContentLoadingController()
+        XCTAssertEqual(controller.loadingState, .initial)
+        
+        let loadingError = NSError(domain: "ContentLoadingCoordinatorTests", code: 0)
+        
+        let delegate = ClosureContentLoadingControllerDelegate(
+            willBeginLoading: { controller in
+                willBeginLoadingExpectation.fulfill()
+            },
+            didFinishLoadingWithUpdate: { controller, update in
+                didFinishLoadingWithUpdateExpectation.fulfill()
+            })
+
+        controller.delegate = delegate
+        
+        controller.loadContent { (coordinator) in
+            coordinator.done(withError: loadingError)
+        }
+        
+        XCTAssertEqual(controller.loadingState, .loadingContent)
+        
+        waitForExpectations(timeout: 0.1) { error in
+            let _ = delegate // Keep strong reference
+            XCTAssertEqual(controller.loadingState, .error)
+            XCTAssertEqual(controller.loadingError, loadingError)
+        }
+    }
+
+    func testIgnore() {
+
+        let willBeginLoadingExpectation = expectation(description: "Will begin loading")
+        let ignoreExpectation = expectation(description: "Ignore coordinator")
+        
+        let controller = ContentLoadingController()
+        XCTAssertEqual(controller.loadingState, .initial)
+        
+        let delegate = ClosureContentLoadingControllerDelegate(
+            willBeginLoading: { controller in
+                willBeginLoadingExpectation.fulfill()
+            },
+            didFinishLoadingWithUpdate: { _ in
+                XCTFail("Did finish loading should not be called")
+            })
+
+        controller.delegate = delegate
+        
+        controller.loadContent { (coordinator) in
+            coordinator.ignore()
+            ignoreExpectation.fulfill()
+        }
+        
+        XCTAssertEqual(controller.loadingState, .loadingContent)
+        
+        waitForExpectations(timeout: 0.1) { error in
+            let _ = delegate // Keep strong reference
+            XCTAssertEqual(controller.loadingState, .loadingContent)
+        }
+    }
+}
---
+++ DataSourceTests/Tests/ContentLoading/ContentLoadingCoordinatorTests.swift
@@ -0,0 +1,92 @@
+//
+//  ContentLoadingCoordinatorTests.swift
+//  DataSource
+//
+//  Created by Dmytro Anokhin on 12/08/16.
+//  Copyright © 2016 Dmytro Anokhin. All rights reserved.
+//
+
+import XCTest
+@testable import DataSource
+
+
+class ContentLoadingCoordinatorTests: XCTestCase {
+
+    func testDoneWithUpdate() {
+        
+        let updateExpectation = expectation(description: "Update expectation")
+        
+        let coordinator = ContentLoadingCoordinator { (state, error, update) in
+            XCTAssertEqual(state, .contentLoaded)
+            update?()
+        }
+        
+        coordinator.done { 
+            updateExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 0.1, handler: nil)
+    }
+    
+    func testDoneWithError() {
+        
+        let errorExpectation = expectation(description: "Error expectation")
+        let loadingError = NSError(domain: "ContentLoadingCoordinatorTests", code: 0)
+        
+        let coordinator = ContentLoadingCoordinator { (state, error, update) in
+            XCTAssertEqual(state, .error)
+            XCTAssertEqual(loadingError, error)
+            errorExpectation.fulfill()
+        }
+        
+        coordinator.done(withError: loadingError)
+        
+        waitForExpectations(timeout: 0.1, handler: nil)
+    }
+    
+    func testIgnore() {
+        
+        let ignoreExpectation = expectation(description: "Ignore expectation")
+        
+        let coordinator = ContentLoadingCoordinator { (state, error, update) in
+            XCTAssertNil(state)
+            XCTAssertNil(error)
+            XCTAssertNil(update)
+            ignoreExpectation.fulfill()
+        }
+        
+        coordinator.ignore()
+        
+        waitForExpectations(timeout: 0.1, handler: nil)
+    }
+    
+    func testMultipleCalls() {
+
+        // The `ContentLoadingCoordinator` must handle only first call and ignore subsequent calls
+
+        let updateExpectation = expectation(description: "Update expectation")
+        
+        var firstCallHandled = false
+        var secondCallHandled = false
+        
+        let coordinator = ContentLoadingCoordinator { (state, error, update) in
+            XCTAssertEqual(state, .contentLoaded)
+            update?()
+        }
+        
+        coordinator.done {
+            firstCallHandled = true
+            updateExpectation.fulfill()
+        }
+        
+        coordinator.done {
+            secondCallHandled = true
+            updateExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 0.1) { error in
+            XCTAssertTrue(firstCallHandled)
+            XCTAssertFalse(secondCallHandled)
+        }
+    }
+}
